cce012686388ffb2a3a00644adf8c2e8cc9ac72c
214090075@qq.com <@DESKTOP-42IKEP9>
2020-12-05 22:02:08 +0800

    完成可添加多个Entity

--- AddGeometric/Program.cs
+++ AddGeometric/Program.cs
@@ -10,10 +10,36 @@ using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.Geometry;
 using Autodesk.AutoCAD.GraphicsInterface;
 
-namespace AddGeometric
+namespace AddEntityToDocument
 {
     public class Program
     {
+        [CommandMethod("AddLine")]
+        public void AddLine()
+        {
+            Document document = Application.DocumentManager.MdiActiveDocument;
+            Line line = new Line(new Point3d(0, 0, 0), new Point3d(100, 100, 0));
+            document.AddEntityToModelSpace(line);
+        }
+    }
+
+    public static class AddEntity
+    {
+        public static void AddEntityToModelSpace(this Document doc, params Entity[] ents)
+        {
+            Database db = doc.Database;
+            using (Transaction trans = db.TransactionManager.StartTransaction())
+            {
+                BlockTable bt = trans.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+                BlockTableRecord btr = trans.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+                foreach (var ent in ents)
+                {
+                    btr.AppendEntity(ent);
+                    trans.AddNewlyCreatedDBObject(ent, true);
+                }
+                trans.Commit();
+            }
+        }
 
     }
 }
