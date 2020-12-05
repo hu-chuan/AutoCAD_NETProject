951357cfd2cfb5b6593dddfca1b523f58c6d80ad
214090075@qq.com <@DESKTOP-42IKEP9>
2020-12-05 21:50:27 +0800

    添加项目文件。

---
+++ AddGeometric/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.GraphicsInterface;
+
+namespace AddGeometric
+{
+    public class Program
+    {
+
+    }
+}
